22bf0c64c23935a20d58130658a1638d5d64e086
thibault.duforest <thibault.duforest@oscaro.com>
2025-02-27 20:49:05 +0100

    Update configs

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,20 @@
+import { reactRouter } from '@react-router/dev/vite'
+import autoprefixer from 'autoprefixer'
+import tailwindcss from 'tailwindcss'
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import tsconfigPaths from 'vite-tsconfig-paths'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  publicDir: 'resources',
+  css: {
+    postcss: {
+      plugins: [tailwindcss, autoprefixer],
+    },
+    preprocessorOptions: {
+      scss: {
+        api: 'modern-compiler',
+      },
+    },
+  },
+  plugins: [reactRouter(), tsconfigPaths()],
 })
